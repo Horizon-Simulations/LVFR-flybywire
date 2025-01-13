dbe347729d72df2d0cdca8165fb8871fb6d99755
markszutor <szutor.mark@gmail.com>
2025-01-13 13:43:01 +0200

    Updated wasm\systems\a320_systems_wasm\src\lib.rs

--- hsim-a321neo/src/wasm/systems/a320_systems_wasm/src/lib.rs
+++ hsim-a321neo/src/wasm/systems/a320_systems_wasm/src/lib.rs
@@ -24,7 +24,9 @@ use reversers::reversers;
 use rudder::rudder;
 use spoilers::spoilers;
 use std::error::Error;
-use systems::air_conditioning::{acs_controller::AcscId, Channel, ZoneType};
+use systems::air_conditioning::{
+    acs_controller::AcscId, cabin_pressure_controller::CpcId, Channel, ZoneType,
+};
 use systems::failures::FailureType;
 use systems::shared::{
     AirbusElectricPumpId, AirbusEngineDrivenPumpId, ElectricalBusType, GearActuatorId,
@@ -91,6 +93,11 @@ async fn systems(mut gauge: msfs::Gauge) -> Result<(), Box<dyn Error>> {
         (21_012, FailureType::CabinFan(1)),
         (21_013, FailureType::CabinFan(2)),
         (21_014, FailureType::GalleyFans),
+        (21_015, FailureType::CpcFault(CpcId::Cpc1)),
+        (21_016, FailureType::CpcFault(CpcId::Cpc2)),
+        (21_017, FailureType::OutflowValveFault),
+        (21_018, FailureType::SafetyValveFault),
+        (21_019, FailureType::RapidDecompression),
         (24_000, FailureType::TransformerRectifier(1)),
         (24_001, FailureType::TransformerRectifier(2)),
         (24_002, FailureType::TransformerRectifier(3)),
@@ -296,6 +303,9 @@ async fn systems(mut gauge: msfs::Gauge) -> Result<(), Box<dyn Error>> {
     .provides_aircraft_variable("AMBIENT WIND Y", "meter per second", 0)?
     .provides_aircraft_variable("AMBIENT WIND Z", "meter per second", 0)?
     .provides_aircraft_variable("ANTISKID BRAKES ACTIVE", "Bool", 0)?
+    .provides_aircraft_variable("CONTACT POINT COMPRESSION", "Percent", 0)?
+    .provides_aircraft_variable("CONTACT POINT COMPRESSION", "Percent", 1)?
+    .provides_aircraft_variable("CONTACT POINT COMPRESSION", "Percent", 2)?
     .provides_aircraft_variable("EXTERNAL POWER AVAILABLE", "Bool", 1)?
     .provides_aircraft_variable("FUEL TANK CENTER QUANTITY", "gallons", 0)?
     .provides_aircraft_variable("FUEL TANK LEFT MAIN QUANTITY", "gallons", 0)?
